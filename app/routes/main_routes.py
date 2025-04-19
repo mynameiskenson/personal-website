10ae5e10c3d0bda299bfa0ccf2d43d1ccd76ca6d
Kenson Manduyag <kenson.manduyag@collectivesolution.net>
2025-04-19 15:30:02 +0800

    Add new routes

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -2,7 +2,18 @@ from flask import Blueprint, render_template, request
 from datetime import datetime
 main = Blueprint("main", __name__)
 
+@main.app_context_processor
+def inject_year():
+    return {"year": datetime.now().year}
+
 @main.route("/")
 def home():
-    _year = datetime.now().year
-    return render_template("home.html", year=_year)
+    return render_template("home.html")
+
+@main.route("/portfolios")
+def portfolios():
+    return render_template("portfolios.html")
+
+@main.route("/blog")
+def blog():
+    return render_template("blog.html")
