b7cff1f0c92acb287c9899ac8d99234b4641884a
Kenson Manduyag <kenson.manduyag@collectivesolution.net>
2025-04-19 15:24:48 +0800

    Setup templating pages

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request
+from datetime import datetime
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template("index.html")
+    _year = datetime.now().year
+    return render_template("home.html", year=_year)
