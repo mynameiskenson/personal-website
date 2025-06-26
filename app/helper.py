0f47573a33dfd8715eca22230bad807bb55a4805
Kenson Manduyag <72254219+mynameiskenson@users.noreply.github.com>
2025-06-27 01:05:48 +0800

    Delete app directory

--- app/helper.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-def gravatar_url(email, size=100, default="retro", rating="g"):
-    email = (email or "").strip().lower().encode("utf=8")
-    hash_email = hashlib.md5(email).hexdigest()
-    return f"https://www.gravatar.com/avatar/{hash_email}?s={size}&d={default}&r={rating}"
